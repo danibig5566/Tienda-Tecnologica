341f8d2a00ca0f8d960d79b8a86e731f7091144c
danibig5566 <156295329+danibig5566@users.noreply.github.com>
2025-05-01 00:36:52 -0500

    se agragaron nuevas cosas como cadena de conexion

--- TiendaBackendApi/TiendaBackendApi/Program.cs
+++ TiendaBackendApi/TiendaBackendApi/Program.cs
@@ -9,33 +9,43 @@ namespace TiendaBackendApi
     {
         public static void Main(string[] args)
         {
-            //configuracion de mi servicio DB
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            
 
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Configuración de CORS
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://localhost:5173")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                                  });
+            });
+
+            // Configuración de base de datos
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+           
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            var app = builder.Build(); 
 
-            // Configure the HTTP request pipeline.
+           
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(MyAllowSpecificOrigins); 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
-        }
-    }
-}
