acde1e7d98cadc270976d381f7324b2a3ff5c74c
danibig5566 <156295329+danibig5566@users.noreply.github.com>
2025-05-09 01:20:05 -0500

    agrgando cmabios

--- TiendaBackendApi/TiendaBackendApi/Program.cs
+++ TiendaBackendApi/TiendaBackendApi/Program.cs
@@ -29,6 +29,7 @@ namespace TiendaBackendApi
 
             builder.Services.AddDbContext<AppDbContext>(options =>
                   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("DevPolicy",
--- TiendaBackendApi/TiendaBackendApi/obj/Debug/net8.0/TiendaBackendApi.AssemblyInfo.cs
+++ TiendaBackendApi/TiendaBackendApi/obj/Debug/net8.0/TiendaBackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaBackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e91cc508ac830abe70bac2c6ab8e979150dc45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41044a123b87e7d5a5a74a8c05fcc94deabcea3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaBackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaBackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
